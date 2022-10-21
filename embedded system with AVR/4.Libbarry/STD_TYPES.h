58528865b028b6ac17372c3e9091b76e94bf554e
Reda Galal <105167134+redagala1@users.noreply.github.com>
2022-10-21 11:31:17 +0200

    lib

---
+++ embedded system with AVR/4.Libbarry/STD_TYPES.h
@@ -0,0 +1,26 @@
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+#define NULL 0
+
+typedef unsigned char u8;
+typedef unsigned short int u16; 
+typedef unsigned long int u32;   // long int = int =4 byte = 32 bit 
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed int s32;
+typedef signed long long int s64;
+
+typedef float f32;
+typedef double f64;
+
+typedef enum
+{
+FALSE,
+TRUE		
+}bool;
+
+#define NULL (void *)0 
+
+#endif
